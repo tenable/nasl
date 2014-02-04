604e27ae718647f33e9497203158395334036b68
Mak Kolybabi <mkolybabi@tenable.com>
2014-02-04 15:57:34 -0500

    Moved all function call tests together.

--- test/unit/parser/test_call.rb
+++ test/unit/parser/test_call.rb
@@ -27,6 +27,59 @@
 class TestCall < Test::Unit::TestCase
   include Nasl::Test
 
+  def test_keyword_prefix
+    # We never want to have a function with the same name as a keyword.
+    fail_parse("if();")
+    fail_parse("break();")
+    fail_parse("continue();")
+    fail_parse("else();")
+    fail_parse("export();")
+    fail_parse("for();")
+    fail_parse("foreach();")
+    fail_parse("function();")
+    fail_parse("global_var();")
+    fail_parse("if();")
+    fail_parse("import();")
+    fail_parse("include();")
+    fail_parse("local_var();")
+    fail_parse("repeat();")
+    fail_parse("return();")
+    fail_parse("until();")
+    fail_parse("while();")
+
+    # We never want to have a function with the same name as a constant.
+    fail_parse("FALSE();")
+    fail_parse("NULL();")
+    fail_parse("TRUE();")
+
+    # X is an exception. It is a valid function name, despite being a keyword.
+    pass("x();")
+
+    # Having a keyword at the start of a function name is perfectly valid.
+    pass("if_();")
+    pass("break_();")
+    pass("continue_();")
+    pass("else_();")
+    pass("export_();")
+    pass("for_();")
+    pass("foreach_();")
+    pass("function_();")
+    pass("global_var_();")
+    pass("if_();")
+    pass("import_();")
+    pass("include_();")
+    pass("local_var_();")
+    pass("repeat_();")
+    pass("return_();")
+    pass("until_();")
+    pass("while_();")
+
+    # Having a constant at the start of a function name is perfectly valid.
+    pass("FALSE_();")
+    pass("NULL_();")
+    pass("TRUE_();")
+  end
+
   def test_no_args
     tree = parse("foo();")
     assert_not_nil(tree)
